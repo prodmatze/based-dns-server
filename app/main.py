a5981ecd5f8d46b7d56629412af41d56cfc44037
matze <104998443+prodmatze@users.noreply.github.com>
2025-05-02 14:19:15 +0300

    corrected error in rfdata length

--- app/main.py
+++ app/main.py
@@ -103,8 +103,8 @@ def parse_answer(query, offset):
     rdlength = struct.unpack("!H", query[offset:offset+2])[0]
     offset += 2
 
-    rdata = struct.unpack("!H", query[offset:offset+2])[0]
-    offset += 2
+    rdata = struct.unpack("!H", query[offset:offset+rdlength])[0]
+    offset += rdlength
 
     answer = {
         "name": answer_name,
